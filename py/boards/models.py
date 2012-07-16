2e2e71738fd2f7b47e29f75e232f3279ff956787
Pam Selle <pamela.selle@gmail.com>
2012-07-16 19:59:52 -0400

    Model update, git ignore update, app update

--- py/boards/models.py
+++ py/boards/models.py
@@ -2,21 +2,21 @@ from django.db import models
 
 class Board(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField
-    cards = ManytoManyField(CardPosition)
-    tags = ManytoManyField(Tag)
+    description = models.TextField()
+    cards = models.ManyToManyField('CardPosition')
+    tags = models.ManyToManyField('Tag')
 
 class CardPosition(models.Model):
-    card = models.ForeignKey(Card)
+    theCard = models.ForeignKey('Card')
     locationData = models.IntegerField()
 
 class Card(models.Model):
-    locations = ManyToManyField(CardPosition)
+    locations = models.ManyToManyField('CardPosition')
     # image
     source = models.CharField(max_length=500)
-    tags = ManytoManyField(Tag)
+    tags = models.ManyToManyField('Tag')
 
 class Tag(models.Model):
     name = models.CharField(max_length=200)
-    boards = ManytoManyField(Board)
-    cards = ManytoManyField(Card)
+    boards = models.ManyToManyField('Board')
+    cards = models.ManyToManyField('Card')
