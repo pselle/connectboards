3b2fd24286910e83364c1d291f08eb8c4dcec573
Pam Selle <pamela.selle@gmail.com>
2012-07-16 19:43:44 -0400

    Add tags

--- py/boards/models.py
+++ py/boards/models.py
@@ -2,7 +2,21 @@ from django.db import models
 
 class Board(models.Model):
     title = models.CharField(max_length=200)
-    cards = ManytoManyField(Card)
+    description = models.TextField
+    cards = ManytoManyField(CardPosition)
+    tags = ManytoManyField(Tag)
+
+class CardPosition(models.Model):
+    card = models.ForeignKey(Card)
+    locationData = models.IntegerField()
 
 class Card(models.Model):
-    boards = ManyToManyField(Board)
+    locations = ManyToManyField(CardPosition)
+    # image
+    source = models.CharField(max_length=500)
+    tags = ManytoManyField(Tag)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    boards = ManytoManyField(Board)
+    cards = ManytoManyField(Card)
